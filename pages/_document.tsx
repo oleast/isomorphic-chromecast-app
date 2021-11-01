025b3f6f83b3f605778e2e6c6846851188ec5ef9
oleast <ole.stokker@bekk.no>
2021-11-01 23:09:38 +0100

    feat(test-chromecast): test some logging of chromecast context

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,10 @@
 import DefaultDocument, { Head, Main, NextScript, Html } from 'next/document';
 
-import { CHROMECAST_APP_ID } from 'common/constants';
-
 class Document extends DefaultDocument {
   render() {
     return (
       <Html lang="no">
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window['__onGCastApiAvailable'] = function(isAvailable) {
-                if (isAvailable) {
-                  initializeCastApi();
-                }
-              };
-          `,
-            }}
-          />
-          <script src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1" />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              initializeCastApi = function() {
-                  cast.framework.CastContext.getInstance().setOptions({
-                    receiverApplicationId: "${CHROMECAST_APP_ID}",
-                    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
-                  });
-                };
-              `,
-            }}
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
