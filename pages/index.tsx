4dc6eb221c5f64d9d5db5c2a1166a97378063a0c
oleast <ole.stokker@bekk.no>
2021-11-07 12:00:07 +0100

    feat(logo): add first iteration of animated logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import { LogoAnimated } from 'components/brand/LogoAnimated';
 import { FC } from 'react';
 
+import _s from './index.module.scss';
+
 const IndexPage: FC = () => {
-  return <></>;
+  return (
+    <div className={_s.container}>
+      <LogoAnimated className={_s.logo} />
+    </div>
+  );
 };
 
 export default IndexPage;
