0672ee3f4356c8369a32d9c0c904bf5993ddeaa1
oleast <ole.stokker@bekk.no>
2021-11-13 15:40:15 +0100

    feat(utils): add more random generator utils

--- utils/random.ts
+++ utils/random.ts
@@ -1,9 +1,41 @@
 import { v4 as uuid4 } from 'uuid';
 
-export const selectRandom = <TValue>(iterable: Iterable<TValue>): TValue => {
+export const randomInInterval = (min: number, max: number): number => {
+  return Math.random() * (max - min) + min;
+};
+
+const getRandomIndex = (max: number) => {
+  return Math.floor(randomInInterval(0, max));
+};
+
+let prevValue: unknown | null = null;
+
+function tryAgainIfEqual<TReturnValue>(
+  callback: () => TReturnValue
+): TReturnValue {
+  const returnValue = callback();
+  if (returnValue === prevValue) {
+    return tryAgainIfEqual(callback);
+  } else {
+    prevValue = returnValue;
+    return returnValue;
+  }
+}
+
+type RandomMode = 'random' | 'uniform';
+
+export const selectRandom = <TValue>(
+  iterable: Iterable<TValue>,
+  mode: RandomMode = 'random'
+): TValue => {
   const array: TValue[] = Array.from(iterable);
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
+  if (mode === 'random') {
+    const randomIndex = getRandomIndex(array.length);
+    return array[randomIndex];
+  } else {
+    const randomIndex = tryAgainIfEqual(() => getRandomIndex(array.length));
+    return array[randomIndex];
+  }
 };
 
 export const generateUniqueId = (): string => {
